6e239b4e9dde0c6eac5afb41e59a1fdb32206355
Wladimir Babitzki <wbabitzki@gmail.com>
2019-10-11 16:43:51 +0200

    introduced unit tests with jest

---
+++ tests/unit/App.spec.js
@@ -0,0 +1,46 @@
+import { shallowMount } from '@vue/test-utils';
+import App from '@/App';
+import FileUploader from "@/components/FileUploader.vue";
+
+let testee;
+
+beforeEach(() => {
+  testee = shallowMount(App, {
+      propsData: {},
+      mocks: {},
+      stubs: {},
+      methods: {},
+  });
+});
+
+afterEach(() => {
+  testee.destroy();
+});
+
+describe('App initial state', () => {
+  test('is a Vue instance', () => {
+    expect(testee.isVueInstance).toBeTruthy();
+  });
+  test ('is FileUploader initially visible', () => {
+    expect(testee.find({ name: "FileUploader" }).isVisible()).toBeTruthy();
+  })
+  test ('is BananaTable initially not visible', () => {
+    expect(testee.find({ name: "BananaTable" }).isVisible()).toBeFalsy();
+  })
+  test ('is BananaConverter initially not visible', () => {
+    expect(testee.find({ name: "BananaConverter" }).isVisible()).toBeFalsy();
+  })
+  test ('is SageTable initially not visible', () => {
+    expect(testee.find({ name: "SageTable" }).isVisible()).toBeFalsy();
+  })
+  test ('is FileDownloader initially not visible', () => {
+    expect(testee.find({ name: "FileDownloader" }).isVisible()).toBeFalsy();
+  })
+});
+
+describe('Upload Banana recoreds', () => {
+  test('Banana table is shown after upload', () => {     
+    testee.find(FileUploader).vm.$emit("uploaded", [ {"uuid": "1ff1325d-8549-490b-9750-ddf05afc90a4", "integratedTransactions": [] } ] );
+    expect(testee.find({ name: "BananaTable" }).isVisible()).toBeTruthy();
+  });
+});
